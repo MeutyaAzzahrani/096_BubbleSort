733eada741c5db6f51edd633d2249e9975bcf3ab
LENOVO <MeutyaAzzahrani>
2024-03-08 16:43:25 +0700

    Deklarasi variabel

--- 096_BubbleSort/096_BubbleSort.cpp
+++ 096_BubbleSort/096_BubbleSort.cpp
@@ -3,4 +3,8 @@
 
 #include <iostream>
 #include <string>
-using namespase
+using namespace std;
+
+int arr[20]; // Deklarasi variabel global array a dengan ukuran 20
+int n; // Deklarasi variabel global n untuk menyimpan banyaknya elemen array
+
